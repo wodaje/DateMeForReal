2156d41acd4fc589ff826929a4d582b627b561d5
Qi Feng <65049795+qifeng86@users.noreply.github.com>
2021-04-19 19:40:58 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,14 @@ import "./index.css";
 import App from "./App";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
+import "./styles/style.css";
+import "./styles/no-touch.min.css";
 
 Amplify.configure(awsconfig);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
