67b86ab849f70e34e95ae877ae09adbb96b4b8b5
Jeff Woda <wodahouse@gmail.com>
2021-04-20 15:28:59 -0400

    updating new fiel home page headline

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,17 +1,14 @@
 import React from "react";
 
 const Home = () => {
-
-    return (
-        <div>
-            <div className="Header">
-                <h1>{"DateMeForReal"}</h1>
-                <p className="line-1 anim-typewriter">your own love guru</p>
-            </div>
-
-        </div>
-    )
-
-}
+  return (
+    <div>
+      <div className="Header">
+        <h1>{"Date4Real"}</h1>
+        <p className="line-1 anim-typewriter">Your Own Luv Guru</p>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
